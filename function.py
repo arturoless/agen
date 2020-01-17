65d1b99e4d922c2ae7217482a8b12c4e97fb2e3e
arturoless <173228@ids.upchiapas.edu.mx>
2020-01-16 23:58:02 -0600

    a

--- function.py
+++ function.py
@@ -4,11 +4,10 @@ import math
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 
 
 probabilidad_mutar_por_individuo = 1/2
-probabilidad_mutar_por_bit = 1/2
 numero_iteraciones = 1000
 poblacion = []
 promedios = []
@@ -58,7 +57,7 @@ for _ in range(numero_iteraciones):
     
     children = []
     for padre in poblacion:
-        parent_1 = padre
+        parent_1 = poblacion[0]
         parent_2 = poblacion[random.randint(0, 15)]
         # rand = random.random()
         # for ind in poblacion:
@@ -106,17 +105,14 @@ for _ in range(numero_iteraciones):
 a = int(poblacion[0].value[0:16], 2)/10000
 b = int(poblacion[0].value[16::], 2)/10000
 valores_y_individuo = []
-sumatoria = 0
 for i in range(len(individual.valores_x)):
-    y_individuo = math.cos(
-        a*individual.valores_x[i])*math.sin(b*individual.valores_x[i])
-    valores_y_individuo.append(y_individuo)
-    sumatoria += (abs(individual.valores_y[i]-y_individuo))
+    y_individuo = math.cos(a*individual.valores_x[i])*math.sin(b*individual.valores_x[i])
+    valores_y_individuo.append(round(y_individuo,4))
 print(valores_y_individuo)
 
 print(poblacion[0])
-print(int(poblacion[0].value[0:16], 2)/10000)
-print(int(poblacion[0].value[16::], 2)/10000)
+print(f'a = {int(poblacion[0].value[0:16], 2)/10000}')
+print(f'b = {int(poblacion[0].value[16::], 2)/10000}')
 
 # def worst_fitness(poblacion):
 #     peor = []
@@ -128,28 +124,29 @@ print(int(poblacion[0].value[16::], 2)/10000)
 def dibujar(x, y, ya):
 
     fig, grafica = plt.subplots(2)
-    grafica[0].set_title('Funcion Y ')
-    grafica[0].plot(y, color="red", label="valores de Y dados")
-    grafica[0].plot(ya, color="black", label="valores de Y encontrados")
+    grafica[0].set_title('Comparación entre datos reales y encontrados')
+    grafica[0].plot(x,y, color="red", label="Y reales")
+    grafica[0].plot(x,ya, color="black", label="Y encontrados")
     grafica[0].legend(bbox_to_anchor=(1, 1), loc='center', borderaxespad=0.)
-    grafica[1].set_title('Fitness')
-    grafica[1].plot(mejores, color="green", label="mejores")
-    grafica[1].plot(promedios, color="blue", label="media")
-    grafica[1].plot(peores, color="red", label="peores")
+    grafica[0].grid()
+    grafica[1].set_title('Evolución de fitness')
+    grafica[1].plot(mejores, color="green", label="Mejor caso")
+    grafica[1].plot(promedios, color="blue", label="Caso promedio")
+    grafica[1].plot(peores, color="red", label="Peor caso")
     grafica[1].legend(bbox_to_anchor=(1, 1), loc='center', borderaxespad=0.)
+    grafica[1].grid()
+    grafica[1].set(xlabel='Generacion', ylabel='Fitness')
+    data = [x,y,ya]
 
-<<<<<<< HEAD
-    data = [y,ya]
-
-    table = plt.table(cellText=data, loc='center')
+    table = plt.table(cellText=data, loc='bottom',rowLabels=['X','Yr','Ya'],bbox=[-0.14,-0.55,1.25,.28])
     table.auto_set_font_size(False)
-=======
-    fig = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
-                                   cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))
-                          ])
->>>>>>> 641ea240f26016d9e141d882319558bec5dba354
+    # fig = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
+    #                                cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))
+    #                       ])
 
-    fig.show()
+    # fig.show()
+    #plt.grid()
+    plt.subplots_adjust(bottom=0.25)
     plt.show()
 
 
